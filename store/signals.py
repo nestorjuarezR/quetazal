5f73a75116869871bceec449b3d636d7216ced3b
Nestor <nesk.company@gmail.com>
2024-02-07 16:29:05 -0600

    Se agrega optimizacion

--- store/signals.py
+++ store/signals.py
@@ -17,13 +17,18 @@ def eliminar_imagen_categoria(sender, instance, **kwargs):
             os.remove(ruta_archivo)
 
 @receiver(pre_delete, sender=Articulo)
-def eliminar_imagen_categoria(sender, instance, **kwargs):
-    # Eliminar la imagen asociada a la categoría cuando la categoría se elimina
+def eliminar_imagenes_articulo(sender, instance, **kwargs):
+    # Eliminar la imagen principal del artículo
     if instance.imagen:
-        # Obtener la ruta completa del archivo
         ruta_archivo = instance.imagen.path
-
-        # Eliminar el archivo si existe
         if os.path.isfile(ruta_archivo):
             os.remove(ruta_archivo)
+
+    # Eliminar todas las imágenes adicionales asociadas al artículo
+    for imagen_producto in instance.imagenes.all():
+        if imagen_producto.imagen:
+            ruta_archivo = imagen_producto.imagen.path
+            if os.path.isfile(ruta_archivo):
+                os.remove(ruta_archivo)
+
 
